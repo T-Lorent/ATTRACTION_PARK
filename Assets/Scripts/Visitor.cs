a5f1a39d5ae29965a86c1f3fed9964a35c8f0509
TLORENT <lorent.tanguy@gmail.com>
2022-12-07 12:26:59 +0100

    ✨ Implementation of State pattern in Visitor

--- Assets/Scripts/Visitor.cs
+++ Assets/Scripts/Visitor.cs
@@ -5,30 +5,58 @@ public class Visitor : MonoBehaviour
 {
 
     /*====== PRIVATE ======*/
-    private Attraction attraction_destination;
-    private NavMeshAgent nav_mesh_agent = null;
+    private enum State
+    {
+        READY_TO_GO,
+        WALKING,
+        WAITING,
+        IN_ATTRACTION
+    }
+    private State _state;
+
+    private Attraction _attraction_destination;
+    private NavMeshAgent _nav_mesh_agent = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        nav_mesh_agent = this.GetComponent<NavMeshAgent>();
-        SetDestinationToNewAttraction();
+        _state = State.READY_TO_GO;
+        _nav_mesh_agent = this.GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (_state)
+        {
+            case State.READY_TO_GO:
+                SetDestinationToNewAttraction();
+                _state = State.WALKING;
+                break;
+
+            case State.WALKING:
+                break;
+
+            case State.WAITING:
+                break;
+            
+            case State.IN_ATTRACTION:
+                break;
+
+            default:
+                Debug.Log("Invalid Visitor state");
+                break;
+        }   
     }
 
     private void SetDestinationToNewAttraction()
     {
-        attraction_destination = AttractionsManager.GetRandomAttraction();
-        SetDestination(attraction_destination.GetEffectiveEntrance());
+        _attraction_destination = AttractionsManager.GetRandomAttraction();
+        SetDestination(_attraction_destination.GetEffectiveEntrance());
     }
 
     private void SetDestination(Vector3 destination)
     {
-        nav_mesh_agent.SetDestination(destination);
+        _nav_mesh_agent.SetDestination(destination);
     }
 }
