0f9d133bb5c203c7f77300b6bfcea2ce000a51d6
badbois <Jordan.roberty@outlook.fr>
2022-12-07 10:10:34 +0100

    ♻️ Move Spawnpoint responsability to agentManager

--- Assets/Scripts/AgentsManager.cs
+++ Assets/Scripts/AgentsManager.cs
@@ -24,10 +24,14 @@ public class AgentsManager : MonoBehaviour
     /*====== PRIVATE ======*/
     private List<GameObject> walkers = new List<GameObject>();
     private List<GameObject> visitors = new List<GameObject>();
+    private List<Vector3> POIs_position = new List<Vector3>();
+
+    private static Vector3 spawn_origin = new Vector3(0, 0, 0);
+    private static float spawn_range = 300;
     
-    private void CreateAgent(GameObject agent_mesh){
-        GameObject new_agent = Instantiate(agent_mesh);
-        new_agent.transform.parent = transform;
+    void CreateAgent(GameObject agent_mesh){
+        Vector3 spawn_position = GetRandomNavMeshPosition();
+        GameObject new_agent = Instantiate(agent_mesh, spawn_position, Quaternion.identity, transform);
 
         if(agent_mesh == walker_mesh) {
             new_agent.GetComponent<Renderer>().material.color = Color.red;
@@ -42,10 +46,10 @@ public class AgentsManager : MonoBehaviour
     void Start()
     {
         // POIs
-        // foreach (Transform POI in Pois)
-        // {
-            
-        // }
+        foreach (Transform POI in POIs.transform)
+        {
+            POIs_position.Add(POI.transform.position);
+        }
 
         // AGENTS
         for(int i=0; i < walker_number; ++i)
@@ -75,5 +79,21 @@ public class AgentsManager : MonoBehaviour
         {
             CreateAgent(visitor_mesh);
         }
+    }
+
+     public static Vector3 GetRandomNavMeshPosition()
+    {
+        //1. Pick a point
+        Vector3 point_on_sphere = Vector3.zero;
+        NavMeshHit hit;
+        Vector3 result = Vector3.zero;
+
+        do {
+            point_on_sphere = spawn_origin + Random.insideUnitSphere * spawn_range;
+        }while(!NavMesh.SamplePosition(point_on_sphere, out hit, 1.0f, NavMesh.AllAreas));
+
+        result = hit.position;
+
+        return result;
     }
 }
--- Assets/Scripts/Visitor.cs
+++ Assets/Scripts/Visitor.cs
@@ -3,8 +3,6 @@ using UnityEngine.AI;
 
 public class Visitor : MonoBehaviour
 {
-    /*====== PUBLIC ======*/
-    public GameObject POIs;
 
     /*====== PRIVATE ======*/
     private NavMeshAgent nav_mesh_agent = null;
@@ -16,12 +14,6 @@ public class Visitor : MonoBehaviour
     {
         nav_mesh_agent = this.GetComponent<NavMeshAgent>();
 
-        Vector3 spawn_origin = new Vector3(0, 0, 0);
-        float spawn_range = 300;
-
-        Vector3 start_position = GetRandomNavMeshPosition(spawn_origin, spawn_range);
-        transform.position = start_position;
-
         Vector3 destination_position = set_destination();
         nav_mesh_agent.SetDestination(destination_position);
     }
@@ -32,29 +24,13 @@ public class Visitor : MonoBehaviour
         
     }
 
-    private Vector3 GetRandomNavMeshPosition(Vector3 origin, float range)
-    {
-        //1. Pick a point
-        Vector3 point_on_sphere = Vector3.zero;
-        NavMeshHit hit;
-        Vector3 result = Vector3.zero;
-
-        do {
-            point_on_sphere = origin + Random.insideUnitSphere * range;
-        }while(!NavMesh.SamplePosition(point_on_sphere, out hit, 1.0f, NavMesh.AllAreas));
-
-        result = hit.position;
-
-        return result;
-    }
-
     private Vector3 set_destination()
     {
-        // Pick a POI
-        int number_of_POI = POIs.transform.childCount;
-        int random_index = Random.Range(0, number_of_POI);
-        Transform POI = POIs.transform.GetChild(random_index);
-        
-        return POI.transform.position;
+        // // Pick a POI
+        // int number_of_POI = POIs.transform.childCount;
+        // int random_index = Random.Range(0, number_of_POI);
+        // Transform POI = POIs.transform.GetChild(random_index);
+        // return POI.transform.position;
+        return new Vector3(0,0,0);
     }
 }
