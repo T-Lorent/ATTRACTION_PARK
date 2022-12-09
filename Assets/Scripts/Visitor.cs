5ce78a5eb8a18e6233aa8aaba8135fca855324ab
TLORENT <lorent.tanguy@gmail.com>
2022-12-09 15:24:02 +0100

    ♻️ Reorganizing State in Visitor

--- Assets/Scripts/Visitor.cs
+++ Assets/Scripts/Visitor.cs
@@ -9,6 +9,7 @@ public class Visitor : MonoBehaviour
     {
         READY_TO_GO,
         WALKING,
+        ARRIVED,
         WAITING,
         IN_ATTRACTION
     };
@@ -29,18 +30,13 @@ public class Visitor : MonoBehaviour
         switch (_state)
         {
             case State.READY_TO_GO:
-                SetDestinationToNewAttraction();
-                _state = State.WALKING;
+                SetState(State.WALKING);
                 break;
 
             case State.WALKING:
                 Vector3 queue_position_position = AttractionsManager.Instance.attractions[_attraction_id].GetQueuePosition();
 
-                if(IsArrived())
-                {
-                    _state = State.WAITING;
-                }
-                else if ((_nav_mesh_agent.destination - queue_position_position).sqrMagnitude > 0.05)
+                if ((_nav_mesh_agent.destination - queue_position_position).sqrMagnitude > 0.05)
                 {
                     SetDestination(queue_position_position);
                 }
@@ -56,41 +52,48 @@ public class Visitor : MonoBehaviour
             default:
                 Debug.Log("Invalid Visitor state");
                 break;
-        }   
+        }
+    }
+
+    private void SetState(State requested_state)
+    {
+        switch (requested_state)
+        {
+            case State.WALKING:
+                if(_state == State.READY_TO_GO) SetDestinationToNewAttraction();
+                _state = State.WALKING;
+                break;
+
+            case State.ARRIVED:
+                SetDestination(transform.position);
+                _state = State.ARRIVED;
+                break;
+
+            default:
+                _state = requested_state;
+                break;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.TryGetComponent(out QueuePosition queue_end))
         {
-            if(queue_end.GetAttractionId() == _attraction_id) SetDestination(transform.position);
+            if (queue_end.GetAttractionId() == _attraction_id)
+            {
+                SetState(State.ARRIVED);
+            }
         }
     }
 
     private void SetDestinationToNewAttraction()
     {
         _attraction_id = AttractionsManager.Instance.GetRandomAttractionId();
-        SetDestination(AttractionsManager.Instance.attractions[_attraction_id].GetQueuePosition());
+        _nav_mesh_agent.SetDestination(AttractionsManager.Instance.attractions[_attraction_id].GetQueuePosition());
     }
 
     private void SetDestination(Vector3 destination)
     {
         _nav_mesh_agent.SetDestination(destination);
-    }
-
-    private bool IsArrived()
-    {
-        if (!_nav_mesh_agent.pathPending)
-        {
-            if (_nav_mesh_agent.remainingDistance <= _nav_mesh_agent.stoppingDistance)
-            {
-                if (!_nav_mesh_agent.hasPath || _nav_mesh_agent.velocity.sqrMagnitude == 0f)
-                {
-                    // Done
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 }
