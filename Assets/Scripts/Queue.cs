9f5127eb6f338134030963d17673a9504cd89bf3
TLORENT <lorent.tanguy@gmail.com>
2022-12-10 20:28:59 +0100

    💥 💩 First dirty implementation of StepBackQueueEnd

--- Assets/Scripts/Queue.cs
+++ Assets/Scripts/Queue.cs
@@ -33,7 +33,7 @@ public class Queue : MonoBehaviour
     public GameObject GetFirstInLine()
     {
         GameObject first_in_line = _waiting_visitors.Dequeue();
-        UpdateQueuePosition(first_in_line.transform.position);
+        Advance(first_in_line.transform.position);
         return first_in_line;
     }
 
@@ -52,24 +52,56 @@ public class Queue : MonoBehaviour
             else
             {
                 _waiting_visitors.Enqueue(visitor_gameObject);
-            }
-
-            // Replacing queue position
-            if (ContainsVisitors())
-            {
-                Vector3 direction = -new_visitor.transform.forward * 5.0F;
-                transform.position = new_visitor.transform.position;
-                if (NavMesh.SamplePosition(transform.position + direction, out NavMeshHit hit, 5.0F, NavMesh.AllAreas))
-                {
-                    transform.position = hit.position;
-                }
+                StepBackQueueEnd();
             }
 
             _collider.enabled = true;
         }
     }
 
-    private void UpdateQueuePosition(Vector3 previous_position)
+    private void StepBackQueueEnd()
+    {
+        // Replacing queue position
+        if (_waiting_visitors.Count > 3)
+        {
+            int max_range = _waiting_visitors.Count - 1;
+            GameObject[] visitors = _waiting_visitors.ToArray();
+
+            Vector3 position_1 = visitors[max_range - 2].transform.position;
+            Vector3 position_2 = visitors[max_range - 1].transform.position;
+            Vector3 position_3 = visitors[max_range].transform.position;
+
+            Vector3 direction_1_to_2 = (position_2 - position_1).normalized;
+            Vector3 direction_1_to_3 = (position_3 - position_1).normalized;
+
+            Quaternion z_rotation = Quaternion.FromToRotation(
+                new Vector3(direction_1_to_2.x, 0, direction_1_to_2.z),
+                new Vector3(direction_1_to_3.x, 0, direction_1_to_3.z)
+            );
+
+            transform.position = position_3;
+            transform.rotation *= Quaternion.FromToRotation(-transform.forward, position_2) * z_rotation;
+            transform.Translate(transform.forward * 2.0F);
+            if (NavMesh.SamplePosition(transform.position, out NavMeshHit hit, 5.0F, NavMesh.AllAreas))
+            {
+                transform.position = hit.position;
+            }
+            transform.rotation = _attraction.transform.rotation;
+        }
+        else
+        {
+            GameObject[] visitors = _waiting_visitors.ToArray();
+            GameObject new_visitor = visitors[_waiting_visitors.Count -1];
+            Vector3 direction = -new_visitor.transform.forward * 5.0F;
+            transform.position = new_visitor.transform.position;
+            if (NavMesh.SamplePosition(transform.position + direction, out NavMeshHit hit, 5.0F, NavMesh.AllAreas))
+            {
+                transform.position = hit.position;
+            }
+        }
+    }
+
+    private void Advance(Vector3 previous_position)
     {
         if(!ContainsVisitors())
         {
