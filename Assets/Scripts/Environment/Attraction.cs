af17d8476d85a75b9f37de74cb3439e6292bf49e
TLORENT <lorent.tanguy@gmail.com>
2023-01-18 09:49:47 +0100

    ♻️ [Assets/*] Improve folder organisation

---
+++ Assets/Scripts/Environment/Attraction.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Attraction : MonoBehaviour
+{
+    [SerializeField] private Transform _entrance;
+    [SerializeField] private Queue _queue;
+    [SerializeField] private Transform _exit;
+    [SerializeField] private int _visitors_capacity = 1;
+    [SerializeField] private Queue<Visitor> _current_visitors = new Queue<Visitor>();
+    [SerializeField] private float _duration = 5F;
+
+    private int _id;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (NavMesh.SamplePosition(_entrance.position, out NavMeshHit entrance_hit, 10.0F, NavMesh.AllAreas))
+        {
+            _entrance.position = entrance_hit.position;
+            _queue.transform.position = entrance_hit.position;
+        }
+        if(NavMesh.SamplePosition(_exit.position, out NavMeshHit exit_hit, 10.0F, NavMesh.AllAreas)) _exit.position = exit_hit.position;
+    }
+
+    void Update()
+    {
+    
+    }
+
+    public int GetId()
+    {
+        return _id;
+    }
+
+    public void SetId(int id)
+    {
+        _id = id;
+    }
+
+    public Vector3 GetEntrancePosition()
+    {
+        return _entrance.position;
+    }
+
+    public Vector3 GetQueuePosition()
+    {
+        return _queue.transform.position;
+    }
+
+    public bool IsFull()
+    {
+        return _current_visitors.Count == _visitors_capacity;
+    }
+
+    public void BringInVisitor(Visitor new_visitor)
+    {
+        new_visitor.SetState(Visitor.State.IN_ATTRACTION);
+        new_visitor.gameObject.SetActive(false);
+        _current_visitors.Enqueue(new_visitor);
+        StartCoroutine("RollVisitors");
+    }
+
+    private IEnumerator RollVisitors()
+    {
+        yield return new WaitForSeconds(_duration);
+        Visitor previous_visitor = _current_visitors.Dequeue();
+        previous_visitor.transform.position = _exit.position;
+        previous_visitor.gameObject.SetActive(true);
+        previous_visitor.SetState(Visitor.State.WALKING);
+
+        if(_queue.ContainsVisitors())
+        {
+            BringInVisitor(_queue.GetFirstInLine());
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/AgentsManager.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentsManager : MonoBehaviour
+{
+    public static AgentsManager Instance;
+
+    /*====== PUBLIC ======*/
+    [Header("AGENT MANAGEMENT")]
+    public Walker walker_prefab;
+    public Visitor visitor_prefab;
+
+    [Header("WALKERS MANAGEMENT (CROWD)")]
+    public int walker_number = 25;
+    public int walker_increment = 25;
+
+    [Header("VISITORS MANAGEMENT")]
+    public int visitor_number = 25;
+    public int visitor_increment = 25;
+
+    /*====== PRIVATE ======*/
+    private List<Walker> walkers = new List<Walker>();
+    private List<Visitor> visitors = new List<Visitor>();
+
+    /*====== UNITY METHODS ======*/
+    void Awake() => Instance = this;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // WALKERS
+        for(int i=0; i < walker_number; ++i)
+        {
+            CreateWalker();
+        }
+
+        // VISITORS
+        for(int i=0; i < visitor_number; ++i)
+        {
+            CreateVisitor();
+        }
+    }
+
+    /*====== METHODS ======*/
+    private void CreateWalker()
+    {
+        Walker new_walker = Instantiate(
+            walker_prefab,
+            GetRandomPosition(),
+            Quaternion.identity,
+            transform
+        );
+
+        walkers.Add(new_walker);
+    }
+
+    private void CreateVisitor()
+    {
+        Visitor new_visitor = Instantiate(
+            visitor_prefab,
+            GetRandomPosition(),
+            Quaternion.identity,
+            transform
+        );
+
+        visitors.Add(new_visitor);
+    }
+
+    public void AddWalkers(){
+        walker_number += walker_increment;
+        
+        for(int i=0; i<walker_increment; ++i)
+        {
+            CreateWalker();
+        }
+    }
+
+    public void AddVisitors(){
+        visitor_number += visitor_increment;
+        
+        for(int i=0; i<visitor_increment; ++i)
+        {
+            CreateVisitor();
+        }
+    }
+
+    public Vector3 GetRandomPosition()
+    {
+        //1. Pick a point
+        float coord_x;
+        float coord_z;
+
+        NavMeshHit hit;
+        Vector3 random_position = Vector3.zero;
+
+        do
+        {
+            coord_x = UnityEngine.Random.Range(0, 400);
+            coord_z = UnityEngine.Random.Range(0, 400);
+        } while (!NavMesh.SamplePosition(new Vector3(coord_x, 20.0F, coord_z), out hit, 20.0f, NavMesh.AllAreas));
+
+        random_position = hit.position;
+
+        return random_position;
+    }
+}
